4266190a0dc7429ff38ca1417ea9079760013f40
Lucas Bretz <94881508+lucasbrtz@users.noreply.github.com>
2022-09-07 09:12:05 -0300

    Delete Recapitulação de Lógica de Programação/Estrutura Sequencial/Exercício1 directory

--- Recapitulação de Lógica de Programação/Estrutura Sequencial/Exercício1/Exercicio1.cs
+++
@@ -1,9 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-int a, b;
-
-Console.Write("Insira o primeiro número inteiro: ");
-a = int.Parse(Console.ReadLine());
-Console.Write("Insira o segundo número inteiro: ");
-b = int.Parse(Console.ReadLine());
-
-Console.WriteLine($"SOMA = {a+b}");
