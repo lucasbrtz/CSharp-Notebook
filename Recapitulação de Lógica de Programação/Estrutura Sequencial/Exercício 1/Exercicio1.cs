a3030db640273fa6512ce4e6ccef5ac41fdd8370
Lucas Bretz <94881508+lucasbrtz@users.noreply.github.com>
2022-09-07 09:12:20 -0300

    Add files via upload

---
+++ Recapitulação de Lógica de Programação/Estrutura Sequencial/Exercício 1/Exercicio1.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a, b;
+
+Console.Write("Insira o primeiro número inteiro: ");
+a = int.Parse(Console.ReadLine());
+Console.Write("Insira o segundo número inteiro: ");
+b = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"SOMA = {a+b}");
